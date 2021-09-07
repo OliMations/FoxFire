ddeed9a1e940239a3b6236240407a9d196037043
Ollie <44728079+OliMations@users.noreply.github.com>
2021-09-07 23:17:42 +0100

    Update C1

--- static/javascript.js
+++ static/javascript.js
@@ -33,9 +33,9 @@ function categoryMenuOpen() {
     }, 900)
 
     // pushes the search bar off screen
-    document.querySelector("#searchButtonHolder").style.transform = "translate(0, -30vh)" 
+    document.querySelector("#searchButtonHolder").style.transform = "translate(0, -32vh)" 
     window.addEventListener("transitionend", function hideSearchBar() {
-        document.querySelector("#searchButtonHolder").style.display = "none"
+        document.querySelector("#searchButtonHolder").style.visibility = "hidden"
         window.removeEventListener('transitionend', hideSearchBar)
     })
     document.querySelector("#searchButtonHolder").style.transitionDelay = "0.52s"
@@ -50,7 +50,7 @@ function categoryMenuClose() {
         // makes the hamburger button reappear after 100ms
         document.querySelector("#hamburger").style.display = "block"
     }, 100)
-    document.querySelector("#searchButtonHolder").style.display = ""
+    document.querySelector("#searchButtonHolder").style.visibility = ""
     document.querySelector("#searchButtonHolder").style.transform = ""
     document.querySelector("#searchButtonHolder").style.transitionDelay = "0s"
 
